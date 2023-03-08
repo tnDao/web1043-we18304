de0e1209edd11ba0c49a5f81261ff3d5ef25f3b9
Dao tn <daotnpd08047@fpt.edu.vn>
2023-03-08 18:00:17 +0700

    nop lap

--- lab1/lab1/3app.js
+++
@@ -1,5 +0,0 @@
-let a = 10;  // set a value of 10 
-// xuất hiện trên cửa sổ console.log
-console.log(a);
-// hiển thị của sổ cảnh báo 
-alert(a)
---
+++ lab1/lab1/app.js
@@ -0,0 +1,8 @@
+// nhap so nguyen a va b
+var a = parseInt(prompt("Nhap so nguyen a "))
+var b = parseInt(prompt("Nhap so nguyen b "))
+// gan bien a b vao c
+var c = a + b
+// xuat ra man hinh
+document.write(c)
+console.log(c);
